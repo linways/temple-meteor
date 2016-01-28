3ba6ca1ab5e92d78fd74e813b5cb542a676f08bc
Rohith Bhaskaran <rohith@linways.com>
2016-01-29 01:11:19 +0530

    editor and save to db completed

--- client/templates/posts/editor.js
+++ client/templates/posts/editor.js
@@ -2,5 +2,48 @@ Template.editor.helpers({
 	expandedPost: function(){
       var postId = Session.get("expandedPostId")
     	return Posts.findOne(postId);
+    },
+    getFEContext: function () {
+    var self = this;
+    return {
+      // Set html content
+      _value: self.body,
+      // Preserving cursor markers
+      _keepMarkers: true,
+      // Override wrapper class
+      _className: "froala-reactive-meteorized-override",
+
+      // Set some FE options
+      toolbarInline: false,
+      initOnClick: false,
+      tabSpaces: false,
+      toolbarButtons:['fullscreen', 'bold', 'italic', 'underline', 'subscript', 'superscript', 'fontFamily', 'fontSize', '|', 'color',  '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', 'insertLink', 'insertTable', 'undo', 'redo', 'clearFormatting', 'html'],
+
+      // FE save.before event handler function:
+      "_onsave.before": function (e, editor) {
+        // Get edited HTML from Froala-Editor
+        var newHTML = editor.html.get(true /* keep_markers */);
+        //var title = $('#title').html();
+        var title = self.title;
+        // Do something to update the edited value provided by the Froala-Editor plugin, if it has changed:
+        if (!_.isEqual(newHTML, self.body)) {
+          $('.fr-toolbar').addClass('body-dirty');
+          Meteor.call('updatePost', self._id, title, newHTML,function(){
+          		console.log("Changes saved");
+          		$('.fr-toolbar').removeClass('body-dirty');
+          });
+        }
+        return false; // Stop Froala Editor from POSTing to the Save URL
+      },
     }
+  },
+});
+
+Template.editor.events({
+	"keyup #title": function(event){
+		var title = $('#title').val();
+		Meteor.call('updatePost', this._id, title, this.body,function(){
+          		console.log("Title Saved");
+          });
+	},
 });
--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,8 +1,22 @@
 Template.postList.helpers({
     publishedPosts: function(){
-      return Posts.find({isPublished: true});
+      return Posts.find({isPublished: true}, {sort: {createdAt: -1}});
     },
     draftPosts: function(){
       return Posts.find({isPublished: false});
     },
+});
+
+Template.postList.events({
+	"click #add-new": function(event){
+		var emptyPost = Posts.findOne({title:'', body:''});
+		if(emptyPost)
+			Session.set('expandedPostId', emptyPost._id);
+		else{
+			Meteor.call('addPost', '', '', function(error, postId){
+				console.log(postId);
+				Session.set('expandedPostId', postId);
+			});
+		}
+	},
 });
